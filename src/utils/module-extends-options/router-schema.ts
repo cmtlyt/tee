900f3899f15313babe401fbf166b4c0d2ab5b313
cmtlyt <95839504+cmtlyt@users.noreply.github.com>
2025-05-10 11:20:56 +0800

    feat: 补充schema工具 (#26)
    * feat: 新增schema工具
    
    * fix: 修复了getObjectType中对properties的错误判断 (#29)
    
    * fix: 修复了getObjectType中对properties的错误判断
    
    * feat: 抽离了isObject方法
    
    * feat: 通过Object.keys遍历替代forin,防止遍历到原型链上的键
    
    ---------
    
    Co-authored-by: cmtlyt <cmtlyt@163.com>
    Co-authored-by: xiaoxin <130760663+hahaXiaoxin@users.noreply.github.com>

--- src/utils/module-extends-options/router-schema.ts
+++ src/utils/module-extends-options/router-schema.ts
@@ -1,4 +1,24 @@
+import type { TypeSpecificSchema } from '../../types/schema-type';
 import type { AppRouterOptions, GetExtendsOptions } from './type';
+import { isObject } from '../object';
+
+/** string 类型 schema */
+const stringType = { type: 'string' } as const;
+/** number 类型 schema */
+const numberType = { type: 'number' } as const;
+/** boolean 类型 schema */
+const booleanType = { type: 'boolean' } as const;
+/** 获取 array 类型 schema */
+function getArrayType(items: TypeSpecificSchema<'array'>['items'], other?: Omit<TypeSpecificSchema<'array'>, 'items'>) {
+  return { type: 'array', items, ...other } as const;
+}
+/** 获取 object 类型 schema */
+function getObjectType<O extends TypeSpecificSchema<'object'>['properties']>(properties: O, required?: (keyof O)[], other?: Omit<TypeSpecificSchema<'object'>, 'properties' | 'required'>) {
+  if (!required && isObject(properties)) {
+    required = Object.keys(properties) as any;
+  }
+  return { type: 'object', properties, required, ...other } as const;
+}
 
 /**
  * router-schema 模块加载时的额外入参, 用于提供辅助方法等
@@ -6,6 +26,11 @@ import type { AppRouterOptions, GetExtendsOptions } from './type';
 export function getRouterSchemaExtendsOptions(_: AppRouterOptions) {
   const handler = {
     prefix: '',
+    stringType,
+    numberType,
+    booleanType,
+    getArrayType,
+    getObjectType,
     setPrefix(prefix: string) {
       this.prefix = prefix;
     },
@@ -15,7 +40,7 @@ export function getRouterSchemaExtendsOptions(_: AppRouterOptions) {
     transform(mod: any) {
       const result: Record<string, any> = {};
       const prefix = this.prefix;
-      for (const key in mod) {
+      for (const key of Object.keys(mod)) {
         result[`${prefix}${key}`] = mod[key];
       }
       return result;
